a4d5c7509acec7077bce8446cacb0243bd103930
paris <parispar@users.noreply.github.com>
2016-04-02 20:54:21 +0300

    Update database configuration

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,11 +17,11 @@ class DatabaseSeeder extends Seeder
         // Running seeds with truncate() will trigger a constraint 
         // violation if foreign key checks are not disabled.
         
-        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
-        foreach($this->toTruncate as $table)  {
-            DB::table($table)->truncate();
-        }
-        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+        // DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        // foreach($this->toTruncate as $table)  {
+        //     DB::table($table)->truncate();
+        // }
+        // DB::statement('SET FOREIGN_KEY_CHECKS = 1');
 
 
         $this->call(UsersTableSeeder::class);
